947c7692ed7aa4de398c2abb2f902dff4e30c40e
FmDG <f.m.degraaf@qmul.ac.uk>
2022-04-05 18:33:01 +0100

    Added first plots to Git

--- U1313_data.py
+++ U1313_data.py
@@ -1,3 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 jakob = pd.read_csv("../data/jakob_2020.csv")
+fig, ax = plt.subplots(2)
+
+ax[1].plot(jakob.age_ka, jakob.d18O)
+ax[0].plot(jakob.age_ka, jakob.MgCa)
+
+plt.show()
