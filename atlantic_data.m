00385f15a548c125355a8eb6dad408d24df03382
FmDG <f.m.degraaf@qmul.ac.uk>
2022-04-06 14:35:59 +0100

    Made Full Data Plotters

--- atlantic_data.m
+++ atlantic_data.m
@@ -2,6 +2,7 @@
 
 % Site U1313
 lang = readtable("data/lang_2016.csv");
+lang_c = readtable("data/lang_2014.csv");
 jakob = readtable("data/jakob_2020.csv");
 bolton = readtable("data/bolton_2011.csv");
 naafs = readtable("data/naafs_2020.csv");
@@ -21,7 +22,7 @@ figure_num = 1;
 % Makes the colours used for the plots
 colours = colormap(hsv(8));
 
-%% Section Three - Plotting the Data
+%% Section Three - Plotting the Full Comparison Data
 
 figure(figure_num)
 figure_num = figure_num + 1;
@@ -29,12 +30,12 @@ figure_num = figure_num + 1;
 subplot(3,1,1);
 
 % Decides on the ages for the graphs
-ages = [2600, 3300];
+ages = [2400, 3400];
 
 % U1313 and 925 d18O
 plot(naafs.age_ka, naafs.d18O, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
 hold on
-plot(wilkens.age_ka, wilkens.d18O, 'color', colours(3, :), 'LineWidth', 1, 'Marker', '+')
+plot(wilkens.age_ka, wilkens.d18O, 'color', colours(6, :), 'LineWidth', 1, 'Marker', '+')
 ylabel("\delta^{18}O", 'fontweight', 'bold')
 ylim([2,5])
 h = gca;  % Handle to currently active axes
@@ -44,18 +45,18 @@ xlabel("Age (ka)", 'fontweight', 'bold');
 title('Corrected \delta^{18}O')
 legend("U1313", "925")
 
-
-% U1313 Nd
+% U1313 and 925 d13C
 subplot(3,1,2);
-
-plot(lang.age_ka, lang.eNd, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
-ylabel("\epsilon_{Nd}", 'fontweight', 'bold')
+plot(lang_c.age_ka, lang_c.d13C, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+hold on
+plot(wilkens.age_ka, wilkens.d13C, 'color', colours(6, :), 'LineWidth', 1, 'Marker', '+')
+ylabel("\delta^{13}C", 'fontweight', 'bold')
 h = gca;  % Handle to currently active axes
-set(h, 'TickDir', 'out');
+set(h,'TickDir', 'out');
 xlim([ages(1), ages(2)]);
 xlabel("Age (ka)", 'fontweight', 'bold');
-title('Corrected \epsilon_{Nd}')
-legend("U1313")
+title('Corrected \delta^{13}C')
+legend("U1313", "925")
 
 % U1313 Mg/Ca
 
@@ -69,6 +70,9 @@ xlim([ages(1), ages(2)]);
 xlabel("Age (ka)", 'fontweight', 'bold');
 title('Mg/Ca')
 legend("U1313")
+
+
+%% Section Four - Plotting the iNHG Data
 
 
 figure(figure_num)
@@ -82,7 +86,7 @@ ages = [2600, 2800];
 % U1313 and 925 d18O
 plot(naafs.age_ka, naafs.d18O, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
 hold on
-plot(wilkens.age_ka, wilkens.d18O, 'color', colours(3, :), 'LineWidth', 1, 'Marker', '+')
+plot(wilkens.age_ka, wilkens.d18O, 'color', colours(6, :), 'LineWidth', 1, 'Marker', '+')
 ylabel("\delta^{18}O", 'fontweight', 'bold')
 ylim([2,5])
 h = gca;  % Handle to currently active axes
@@ -93,17 +97,19 @@ title('Corrected \delta^{18}O')
 legend("U1313", "925")
 
 
-% U1313 Nd
+% U1313 and 925 d13C
 subplot(3,1,2);
-
-plot(lang.age_ka, lang.eNd, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
-ylabel("\epsilon_{Nd}", 'fontweight', 'bold')
+plot(lang_c.age_ka, lang_c.d13C, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+hold on
+plot(wilkens.age_ka, wilkens.d13C, 'color', colours(6, :), 'LineWidth', 1, 'Marker', '+')
+ylabel("\delta^{13}C", 'fontweight', 'bold')
 h = gca;  % Handle to currently active axes
-set(h, 'TickDir', 'out');
+set(h,'TickDir', 'out');
 xlim([ages(1), ages(2)]);
 xlabel("Age (ka)", 'fontweight', 'bold');
-title('Corrected \epsilon_{Nd}')
-legend("U1313")
+title('Corrected \delta^{13}C')
+legend("U1313", "925")
+
 
 % U1313 Mg/Ca
 
@@ -117,3 +123,88 @@ xlim([ages(1), ages(2)]);
 xlabel("Age (ka)", 'fontweight', 'bold');
 title('Mg/Ca')
 legend("U1313")
+
+%% Section Five - Plotting the U1313 Data
+
+figure(figure_num)
+figure_num = figure_num + 1;
+
+
+% Decides on the ages for the graphs
+ages = [2400, 3400];
+
+subplot(2,1,1);
+
+yyaxis left;
+
+plot(lang.age_ka, lang.eNd, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+xlim([ages(1), ages(2)]);
+ylabel("\epsilon_{Nd}", 'Color', colours(1, :), 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'YColor', colours(1, :));
+
+yyaxis right
+plot(lang_c.age_ka, lang_c.d13C, 'color', colours(7, :), 'LineWidth', 1, 'Marker', '+')
+xlim([ages(1), ages(2)]);
+ylabel("\delta^{13}C", 'Color', colours(7, :), 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'YColor', colours(7, :));
+subplot(2,1,2);
+
+yyaxis left;
+
+plot(jakob.age_ka, jakob.MgCa, 'color', colours(5, :), 'LineWidth', 1, 'Marker', '+')
+xlim([ages(1), ages(2)]);
+ylabel("Mg/Ca", 'Color', colours(5, :), 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'YColor', colours(5, :));
+
+
+yyaxis right
+plot(lang_c.age_ka, lang_c.d13C, 'color', colours(7, :), 'LineWidth', 1, 'Marker', '+')
+xlim([ages(1), ages(2)]);
+ylabel("\delta^{13}C", 'Color', colours(7, :), 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'YColor', colours(7, :));
+
+
+figure(figure_num)
+figure_num = figure_num + 1;
+
+
+% Decides on the ages for the graphs
+ages = [2400, 3400];
+
+subplot(2,1,1);
+
+yyaxis left;
+
+plot(lang.age_ka, lang.eNd, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+xlim([ages(1), ages(2)]);
+ylabel("\epsilon_{Nd}", 'Color', colours(1, :), 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'YColor', colours(1, :));
+
+yyaxis right
+plot(naafs.age_ka, naafs.d18O, 'color', colours(7, :), 'LineWidth', 1, 'Marker', '+')
+xlim([ages(1), ages(2)]);
+ylabel("\delta^{18}O", 'Color', colours(7, :), 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'YDir', 'reverse', 'YColor', colours(7, :));
+
+subplot(2,1,2);
+
+yyaxis left;
+
+plot(jakob.age_ka, jakob.MgCa, 'color', colours(5, :), 'LineWidth', 1, 'Marker', '+')
+xlim([ages(1), ages(2)]);
+ylabel("Mg/Ca", 'Color', colours(5, :), 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'YColor', colours(5, :));
+
+yyaxis right
+plot(naafs.age_ka, naafs.d18O, 'color', colours(7, :), 'LineWidth', 1, 'Marker', '+')
+xlim([ages(1), ages(2)]);
+ylabel("\delta^{18}O", 'Color', colours(7, :), 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'YDir', 'reverse', 'YColor', colours(7, :));
