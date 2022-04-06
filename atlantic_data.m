1467bc62f7abb058297e2e49c65ff571b42a85e7
FmDG <f.m.degraaf@qmul.ac.uk>
2022-04-06 11:40:31 +0100

    Added data and cleaned up figures

--- atlantic_data.m
+++ atlantic_data.m
@@ -1,7 +1,11 @@
 %% Section One - Reading Data
 
+% Site U1313
 lang = readtable("data/lang_2016.csv");
 jakob = readtable("data/jakob_2020.csv");
+bolton = readtable("data/bolton_2011.csv");
+naafs = readtable("data/naafs_2020.csv");
+% Site 925
 wilkens = readtable("data/wilkens_2017.csv");
 
 %% Section Two - Processing Data
@@ -13,27 +17,103 @@ wilkens = readtable("data/wilkens_2017.csv");
 % [~, ia] = unique(lang);
 % lang = lang(ia , :);
 
-figure_no = 1;
+figure_num = 1;
 % Makes the colours used for the plots
-colours = colormap(hsv(3));
+colours = colormap(hsv(8));
 
 %% Section Three - Plotting the Data
 
-figure(figure_no)
-figure_no = figure_no + 1;
+figure(figure_num)
+figure_num = figure_num + 1;
+
+subplot(3,1,1);
 
 % Decides on the ages for the graphs
-ages = [2600, 3600];
+ages = [2600, 3300];
 
-% Cibicidoides wuellerstorfi d18O
-plot(data_u1308.age_ka, data_u1308.d18O, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+% U1313 and 925 d18O
+plot(naafs.age_ka, naafs.d18O, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
 hold on
-plot(data_u1313.age_ka, data_u1313.d18O, 'color', colours(3, :), 'LineWidth', 1, 'Marker', '+')
+plot(wilkens.age_ka, wilkens.d18O, 'color', colours(3, :), 'LineWidth', 1, 'Marker', '+')
 ylabel("\delta^{18}O", 'fontweight', 'bold')
 ylim([2,5])
 h = gca;  % Handle to currently active axes
 set(h, 'YDir', 'reverse', 'TickDir', 'out');
 xlim([ages(1), ages(2)]);
 xlabel("Age (ka)", 'fontweight', 'bold');
-title('\delta^{18}O from C. wuellerstorfi')
-legend("U1308", "U1313")
+title('Corrected \delta^{18}O')
+legend("U1313", "925")
+
+
+% U1313 Nd
+subplot(3,1,2);
+
+plot(lang.age_ka, lang.eNd, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+ylabel("\epsilon_{Nd}", 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'TickDir', 'out');
+xlim([ages(1), ages(2)]);
+xlabel("Age (ka)", 'fontweight', 'bold');
+title('Corrected \epsilon_{Nd}')
+legend("U1313")
+
+% U1313 Mg/Ca
+
+subplot(3,1,3);
+
+plot(jakob.age_ka, jakob.MgCa, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+ylabel("Mg/Ca", 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'TickDir', 'out');
+xlim([ages(1), ages(2)]);
+xlabel("Age (ka)", 'fontweight', 'bold');
+title('Mg/Ca')
+legend("U1313")
+
+
+figure(figure_num)
+figure_num = figure_num + 1;
+
+subplot(3,1,1);
+
+% Decides on the ages for the graphs
+ages = [2600, 2800];
+
+% U1313 and 925 d18O
+plot(naafs.age_ka, naafs.d18O, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+hold on
+plot(wilkens.age_ka, wilkens.d18O, 'color', colours(3, :), 'LineWidth', 1, 'Marker', '+')
+ylabel("\delta^{18}O", 'fontweight', 'bold')
+ylim([2,5])
+h = gca;  % Handle to currently active axes
+set(h, 'YDir', 'reverse', 'TickDir', 'out');
+xlim([ages(1), ages(2)]);
+xlabel("Age (ka)", 'fontweight', 'bold');
+title('Corrected \delta^{18}O')
+legend("U1313", "925")
+
+
+% U1313 Nd
+subplot(3,1,2);
+
+plot(lang.age_ka, lang.eNd, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+ylabel("\epsilon_{Nd}", 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'TickDir', 'out');
+xlim([ages(1), ages(2)]);
+xlabel("Age (ka)", 'fontweight', 'bold');
+title('Corrected \epsilon_{Nd}')
+legend("U1313")
+
+% U1313 Mg/Ca
+
+subplot(3,1,3);
+
+plot(jakob.age_ka, jakob.MgCa, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+ylabel("Mg/Ca", 'fontweight', 'bold')
+h = gca;  % Handle to currently active axes
+set(h, 'TickDir', 'out');
+xlim([ages(1), ages(2)]);
+xlabel("Age (ka)", 'fontweight', 'bold');
+title('Mg/Ca')
+legend("U1313")
