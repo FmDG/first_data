2a2f5afc5eb026a7624d382d24149cbada3c663c
FmDG <f.m.degraaf@qmul.ac.uk>
2022-04-06 10:50:51 +0100

    added_comments

--- atlantic_data.m
+++ atlantic_data.m
@@ -8,20 +8,22 @@ wilkens = readtable("data/wilkens_2017.csv");
 
 % Remove missing rows, sort the dataset by age, and make sure every value
 % is unique:
-% cib_1209 = rmmissing(cib_1209);
-% cib_1209 = sortrows(cib_1209, "age_ka");
-% [~, ia] = unique(cib_1209);
-% cib_1209 = cib_1209(ia , :);
+% lang = rmmissing(lang);
+% lang = sortrows(lang, "age_ka");
+% [~, ia] = unique(lang);
+% lang = lang(ia , :);
 
-%% Section Three - Plotting the Data
-
+figure_no = 1;
 % Makes the colours used for the plots
 colours = colormap(hsv(3));
+
+%% Section Three - Plotting the Data
 
 figure(figure_no)
 figure_no = figure_no + 1;
 
-ages = [2600, 3200];
+% Decides on the ages for the graphs
+ages = [2600, 3600];
 
 % Cibicidoides wuellerstorfi d18O
 plot(data_u1308.age_ka, data_u1308.d18O, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
