85fb2e58ffe09500eaa07b1199c35a268a2c66cb
FmDG <f.m.degraaf@qmul.ac.uk>
2022-04-07 13:46:15 +0100

    Renamed and rebased python files

--- atlantic_data.py
+++ atlantic_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-jakob = pd.read_csv("../data/jakob_2020.csv")
+jakob = pd.read_csv("data/jakob_2020.csv")
 fig, ax = plt.subplots(2)
 
 ax[1].plot(jakob.age_ka, jakob.d18O)
