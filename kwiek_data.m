77f5c769210c1be774a25237ea5f4bda429c5931
FmDG <f.m.degraaf@qmul.ac.uk>
2022-04-29 12:07:39 +0100

    Added mix_1995 data

--- kwiek_data.m
+++ kwiek_data.m
@@ -3,16 +3,17 @@
 % Site 1014A (1177m depth)
 data1014 = readtable("data/kwiek_1999A.csv");
 
-%Site 1018A (2476m depth)
+% Site 1018A (2476m depth)
 data1018 = readtable('data/kwiek_1999B.csv');
 
-% Makes the colours used for the plots
-colours = colormap(hsv(8));
+% Site 849 (3851m depth)
+data849 = readtable("data/mix_1995.csv");
 
 %% Section Two - Preparing Data
 
 data1014_rm = rmmissing(data1014);
 data1018_rm = rmmissing(data1018);
+data849 = rmmissing(data849);
 
 
 figure_num = 1;
@@ -31,9 +32,9 @@ figure_num = figure_num + 1;
 % Uvig Subplot
 subplot(2,1,1);
 
-plot(data1014.age_ka, data1014.Uvig_d18O, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+plot(data1014.age_ka, data1014.Uvig_d18O, 'color', 'r', 'LineWidth', 1, 'Marker', '+')
 hold on
-plot(data1018.age_ka, data1018.Uvig_d18O, 'color', colours(6, :), 'LineWidth', 1, 'Marker', '+')
+plot(data1018.age_ka, data1018.Uvig_d18O, 'color', 'b', 'LineWidth', 1, 'Marker', '+')
 ylabel("\delta^{18}O", 'fontweight', 'bold')
 % ylim([2,5])
 h = gca;  % Handle to currently active axes
@@ -46,9 +47,10 @@ legend("ODP 1014 (1177m)", "ODP 1018 (2476m)")
 % Cib Subplot
 subplot(2,1,2);
 
-plot(data1014_rm.age_ka, data1014_rm.Cib_d18O, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+plot(data1014_rm.age_ka, data1014_rm.Cib_d18O, 'color', 'r', 'LineWidth', 1, 'Marker', '+')
 hold on
-plot(data1018_rm.age_ka, data1018_rm.Cib_d18O, 'color', colours(6, :), 'LineWidth', 1, 'Marker', '+')
+plot(data1018_rm.age_ka, data1018_rm.Cib_d18O, 'color', 'b', 'LineWidth', 1, 'Marker', '+')
+plot(data849.age_ka, data849.d18O, 'color', [0.25, 0.5, 0.1], 'LineWidth', 1, 'Marker', '+')
 ylabel("\delta^{18}O", 'fontweight', 'bold')
 % ylim([2,5])
 h = gca;  % Handle to currently active axes
@@ -56,7 +58,7 @@ set(h, 'YDir', 'reverse', 'TickDir', 'out');
 xlim([ages(1), ages(2)]);
 xlabel("Age (ka)", 'fontweight', 'bold');
 title('Cibicidoides \delta^{18}O')
-legend("ODP 1014 (1177m)", "ODP 1018 (2476m)")
+legend("ODP 1014 (1177m)", "ODP 1018 (2476m)", "ODP 849 (3851m)")
 
 %% Section Four - d13C Figure
 
@@ -70,9 +72,9 @@ figure_num = figure_num + 1;
 % Uvig Subplot
 subplot(2,1,1);
 
-plot(data1014.age_ka, data1014.Uvig_d13C, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+plot(data1014.age_ka, data1014.Uvig_d13C, 'color', 'r', 'LineWidth', 1, 'Marker', '+')
 hold on
-plot(data1018.age_ka, data1018.Uvig_d13C, 'color', colours(6, :), 'LineWidth', 1, 'Marker', '+')
+plot(data1018.age_ka, data1018.Uvig_d13C, 'color', 'b', 'LineWidth', 1, 'Marker', '+')
 ylabel("\delta^{13}C", 'fontweight', 'bold')
 h = gca;  % Handle to currently active axes
 set(h, 'TickDir', 'out');
@@ -84,14 +86,15 @@ legend("ODP 1014 (1177m)", "ODP 1018 (2476m)")
 % Cib Subplot
 subplot(2,1,2);
 
-plot(data1014_rm.age_ka, data1014_rm.Cib_d13C, 'color', colours(1, :), 'LineWidth', 1, 'Marker', '+')
+plot(data1014_rm.age_ka, data1014_rm.Cib_d13C, 'color', 'r', 'LineWidth', 1, 'Marker', '+')
 hold on
-plot(data1018_rm.age_ka, data1018_rm.Cib_d13C, 'color', colours(6, :), 'LineWidth', 1, 'Marker', '+')
+plot(data1018_rm.age_ka, data1018_rm.Cib_d13C, 'color', 'b', 'LineWidth', 1, 'Marker', '+')
+plot(data849.age_ka, data849.d13C, 'Color', [0.25, 0.5, 0.1], 'LineWidth', 1, 'Marker', '+')
 ylabel("\delta^{13}C", 'fontweight', 'bold')
 h = gca;  % Handle to currently active axes
 set(h, 'TickDir', 'out');
 xlim([ages(1), ages(2)]);
 xlabel("Age (ka)", 'fontweight', 'bold');
 title('Cibicidoides \delta^{13}C')
-legend("ODP 1014 (1177m)", "ODP 1018 (2476m)")
+legend("ODP 1014 (1177m)", "ODP 1018 (2476m)", "ODP 849 (3851m)")
 
